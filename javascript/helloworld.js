7456002a2a4cfeef443b37ec3c3e896d4f2a2966
BhavyaLutukurthi <lutukurthibhavyasri@gmail.com>
2024-10-20 16:58:44 +0530

    initial commit

---
+++ javascript/helloworld.js
@@ -0,0 +1,38 @@
+// Code structure //
+// console.log('Hello World!!');
+// console.log("Hello");
+
+// [1, 2].forEach(console.log);
+
+////// VARIABLE DECLARATION /////
+
+// let name ='Bhavya';
+// let initial;
+// initial = 'Bhavya'
+// console.log(name);
+// console.log(initial);
+
+// let admin ;
+// let name ='John';
+// admin ='John';
+// console.log(admin);
+///////// Data types ////
+// let n = 123;
+// n = 12.23;
+// console.log(n);
+// console.log(1/0); //Infinity
+// console.log(NaN +1);
+// console.log(NaN -1);
+// console.log(NaN * 1);
+// console.log(NaN/1);
+// console.log(NaN ** 0);
+// let n=123456789100n;
+// console.log(typeof(n));
+let name = "John";
+
+// embed a variable
+console.log( `Hello, ${name}!` ); // Hello, John!
+
+// embed an expression
+console.log( `the result is ${1 + 2}` ); // the result is 3
+
